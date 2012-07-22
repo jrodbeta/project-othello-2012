7c99a8ecbbadf970fa29f2bc6b83c701eba5cc19
Ethan Nam <sungju.nam@gmail.com>
2012-07-22 18:41:06 -0400

    Added MobilityHeuristicAI.java.

--- src/othello/ai/GreedyHeuristicAI.java
+++ src/othello/ai/GreedyHeuristicAI.java
@@ -26,45 +26,38 @@ public class GreedyHeuristicAI implements ReversiAI
 	  public Board nextMove(Board prev, int lastx, int lasty)
 	  {
 		  Board b = new Board(prev), best = null;
-		  Board temp;
+		  int color = b.getActive();
+		  int score = -1000;
 		  
-		  int mobility = 0;
-		  int lowestMobility = 100;
-
 		  for(int j = 0; j < size; j++)
-		  {
-			  for(int i = 0; i < size; i++)
-			  {
-				  if(b.move(i, j)) // valid move
-				  {
-					  temp = new Board(b);
-					  
-					  for(int c = 0; c < size; c++)
-					  {
-						  for(int d = 0; d < size; d++)
-						  {
-							  if(temp.move(d, c)) // valid move
-							  {
-								  mobility++;
-							  }
-							  
-							  temp = new Board(b);
-						  }
-					  }
-					  
-					  if(mobility < lowestMobility) {
-						  lowestMobility = mobility;
-						  mobility = 0;
-						  best = b;
-					  }
-					  
-					  b = new Board(prev);
-				  }
-			  }
-		  }
-
-
-		  return best;
+		    {
+		      for(int i = 0; i < size; i++)
+		      {
+		        if(b.move(i, j)) // valid move
+		        {
+		          int newscore = 0;
+		          
+		          for(int y = 0; y < 8; y++)
+		          {
+		            for(int x = 0; x < 8; x++)
+		            {
+		              int state = b.getState(x, y);
+		              if(state == color) newscore += cost(x, y);
+		              else if(state != Board.EMPTY) newscore -= cost(x,y);
+		            }
+		          }
+		          
+		          if(newscore > score)
+		          {
+		            score = newscore;
+		            best = b;
+		            bestMove = new Point(i,j);
+		          }
+		          b = new Board(prev);
+		        }
+		      }
+		    }
+		    return best;
 	  }
 	  
 	  @Override
